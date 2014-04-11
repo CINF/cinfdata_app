8563105b415d0dbdf7932ada82c38fc9218dabf5
Kenneth Nielsen <k.nielsen81@gmail.com>
2014-04-11 12:13:36 +0200

    Fixed plot settings exchange infrastructure

--- cinfdata/main.py
+++ cinfdata/main.py
@@ -35,24 +35,31 @@ class MainCarousel(Carousel):
         # Initiate cinfdata and bind properties
         self.cinfdata = Cinfdata(creds.username, creds.password)
         self.cinfdata.bind(selected_plot=self.change_plot)
-        # Add a reference to cinfdata to objects
+
+        # Add a reference to cinfdata to page selection
         self.ids.page_selection.cinfdata = self.cinfdata
+
+        # Initiate date plot options and add cinfdata reference
         self.dateplot_options = DatePlotOptions()
         self.dateplot_options.cinfdata = self.cinfdata
+
         # This emulates pressing the plot selection button and should set all
         # of the gui up for a specific plot
         self.cinfdata.selected_plot = self.ids.page_selection.first
 
     def on_index(self, *args):
         super(MainCarousel, self).on_index(*args)
+        Logger.debug('MainCarousel: index: {}'.format(args[1]))
 
     def change_plot(self, instance, value):
         """Change the plot settings widget when a new plot is selected"""
-        index = self.index
+        #index = self.index
         if value.plot_type == 'date':
-            self.remove_widget(self.slides[0])
-            self.add_widget(self.dateplot_options, -2)
-            self.index = index
+            self.ids.plot_settings.clear_widgets()
+            self.ids.plot_settings.add_widget(self.dateplot_options)
+            #self.remove_widget(self.slides[0])
+            #self.add_widget(self.dateplot_options, -2)
+            #self.index = index
         else:
             message = 'Support for the \'{}\' plot type is not yet implemented'
             raise(NotImplementedError(message))
